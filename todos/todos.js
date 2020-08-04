652c711feb04cbfe2773b8b9f0db61615078e1b1
unknown <matheus.oliverti@gmail.com>
2020-08-04 19:04:04 -0300

    storage

--- todos/todos.js
+++ todos/todos.js
@@ -2,11 +2,7 @@ var listElement = document.querySelector('#app ul')
 var inputElement = document.querySelector('#app input')
 var buttonElement = document.querySelector('#app button')
 
-var todos = [
-    'Fazer café',
-    'Estudar javaScript',
-    'Acessar comunidade rocketSeat'
-]
+var todos = JSON.parse(localStorage.getItem('list_todos')) || []
 
 function renderTodos(){
     listElement.innerHTML = ''
@@ -41,6 +37,7 @@ function addTodo(){
     todos.push(todoText)
     inputElement.value = ''
     renderTodos()
+    saveToStorage()
 }
 
 buttonElement.onclick = addTodo
@@ -48,5 +45,10 @@ buttonElement.onclick = addTodo
 function deleteTodo(pos){
     todos.splice(pos, 1)
     renderTodos()
+    saveToStorage()
+}
+
+function saveToStorage(){
+    localStorage.setItem('list_todos', JSON.stringify(todos))
 }
 
