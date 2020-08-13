62883fc1751d92f91e6fdeff4dee79a9ff0186b5
matheusoliveiraqa <matheus.silva@fcamara.com.br>
2020-08-13 11:58:16 -0300

    tabela

--- assincrono/atividade/main.js
+++ assincrono/atividade/main.js
@@ -7,33 +7,37 @@ buscar.setAttribute('onclick', `listaRepos()`)
 
 function listaRepos(){
     var usuario = iptUsuario.value
+     axios.get(`https://api.github.com/users/${usuario}/repos`) 
     
-    axios.get(`https://api.github.com/users/${usuario}/repos`)
     // response.status
 
     .then(function(response){
-        var tabela = document.getElementById('tabela')
+        repos.innerHTML = ''
+        var tabela = document.createElement('table')
+        repos.appendChild(tabela)
         tabela.innerHTML = '<tr><th>ID</th><th>Repo</th><th>link</th></tr>'
         for(var value of response.data){
+
             var novaLinha = document.createElement('tr')
-            var novoId = document.createElement('td')
-            var idTexto = document.createTextNode(value.id)
-            var novoNome = document.createElement('td')
-            var nomeTexto = document.createTextNode(value.name)
-            var novoLink = document.createElement('td')
-            var linkTexto = document.createTextNode(value.html_url)
-            var link = document.createElement('a')
-            link.setAttribute('href', value.html_url)
+            novaLinha.innerHTML = `<td>${value.id}</td><td>${value.name}</td><td><a href="${value.html_url}">${value.html_url}</a></td>` 
+            // var novoId = document.createElement('td')
+            // var idTexto = document.createTextNode(value.id)
+            // var novoNome = document.createElement('td')
+            // var nomeTexto = document.createTextNode(value.name)
+            // var novoLink = document.createElement('td')
+            // var linkTexto = document.createTextNode(value.html_url)
+            // var link = document.createElement('a')
+            // link.setAttribute('href', value.html_url)
 
             tabela.appendChild(novaLinha)
-            novaLinha.appendChild(novoId)
-            novoId.appendChild(idTexto)
-            novaLinha.appendChild(novoNome)
-            novoNome.appendChild(nomeTexto)
-            novaLinha.appendChild(novoLink)
-            novoLink.appendChild(link)
-            link.appendChild(linkTexto)
-            console.log(value.html_url)
+            // novaLinha.appendChild(novoId)
+            // novoId.appendChild(idTexto)
+            // novaLinha.appendChild(novoNome)
+            // novoNome.appendChild(nomeTexto)
+            // novaLinha.appendChild(novoLink)
+            // novoLink.appendChild(link)
+            // link.appendChild(linkTexto)
+            // console.log(value.html_url)
         }
         
     })
